70f5a48df28cd22dfcb1e69e296f608051f9c67f
João Felipe Brandão Jatobá <99279259+BrandaoJatoba@users.noreply.github.com>
2022-08-26 10:44:04 -0300

    Update factorRoot.py
    Cleaner message after factor

--- factorRoot.py
+++ factorRoot.py
@@ -1,15 +1,24 @@
-def simplifyRoot(value):
+import copy
+
+
+def simplify_root(value):
     factor = 2
-    simplifiedRadicand = value;
+    simplified_radicand = copy.deepcopy(value)
     root = 1
-    while (factor <= simplifiedRadicand):
-        if (simplifiedRadicand % (factor*factor) == 0):
-            simplifiedRadicand /= (factor * factor)
-            root *= factor;            
+    while factor <= simplified_radicand:
+        if simplified_radicand % (factor*factor) == 0:
+            simplified_radicand /= (factor * factor)
+            root *= factor
         else:
             factor = factor + 1
-    print("Raiz: " + str(root))
-    if (simplifiedRadicand!=1):
-        print("Radicando: " + str(int(simplifiedRadicand)))
+    msg = "A raiz de " + str(value)
+    if simplified_radicand != 1:
+        msg = msg + " simplificada"
+    msg = msg + " é " + str(root)
+    if simplified_radicand != 1:
+        msg = msg + " √" + str(int(simplified_radicand))
+    print(msg)
+
+
 teste = int(input("Insira radicando a ser simplificado:"))
-simplifyRoot(teste)
+simplify_root(teste)
